00f43f6d0b0351f2c148c1cc3abfb65f56c417cc
ISEA <informpranav@gmail.com>
2019-02-25 13:34:52 +0530

    Adding Login and Sign Up Screens

--- app/src/main/java/com/pranav/layoutdemo/SignUpActivity.java
+++ app/src/main/java/com/pranav/layoutdemo/SignUpActivity.java
@@ -1,28 +1,26 @@
 package com.pranav.layoutdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     private Button btnLogin,btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.sign_up_page);
         btnLogin=(Button)findViewById(R.id.btnLogin);
         btnSignup=(Button)findViewById(R.id.btnSignup);
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
                 startActivity(intent);
             }
         });
